d16078c99ad2e0651efc173057b6cb8c09da3502
kaan.bas <baskaan8@gmail.com>
2023-05-22 12:13:21 +0300

    React useState

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
-const Home = () => {
-    const handleClick = () =>{
-        console.log('Hello World!')
-    }
+import { useState } from "react";
 
-    return ( 
-        <div className="Home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick()}></button>
-        </div>
-     );
-}
- 
+
+const Home = () => {
+   const [name,setName] = useState('Kaan');
+   const [age,setAge] = useState(26);
+   const handleClick = () =>{
+    setName('Test')
+    setAge('30')
+   }
+
+  return (
+    <div className="Home">
+      <h2>Home Page</h2>
+      <p>{name} is {age} years old</p>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+};
+
 export default Home;
