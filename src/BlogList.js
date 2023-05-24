61e89448ce84b6f1fda62ea6145f284188e7ddce
kaan.bas <baskaan8@gmail.com>
2023-05-24 14:20:03 +0300

    data json server and fecth loading data

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,6 +2,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
+      <hr/>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title: "My New Website",
-      body: "lorem ipsum ...",
-      author: "person1",
-    },
-    {
-      id: 2,
-      title: "Welcome Party!",
-      body: "lorem ipsum ...",
-      author: "person2",
-    },
-    {
-      id: 3,
-      title: "Hello Worl..!",
-      body: "lorem ipsum ...",
-      author: "person3",
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
+
   const handleDelete = (id) => {
     console.log(blogs);
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
+  const URL = "http://localhost:8000/";
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(URL + "blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setPending(false);
+        });
+    },2000);
+  }, []);
+
   return (
     <div className="Home">
-      <BlogList
-        blogs={blogs}
-        title="All Blogs"
-        handleDelete={handleDelete}
-      ></BlogList>
+      {isPending && <div>Loading...</div>}
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs"
+          handleDelete={handleDelete}
+        ></BlogList>
+      )}
     </div>
   );
 };
